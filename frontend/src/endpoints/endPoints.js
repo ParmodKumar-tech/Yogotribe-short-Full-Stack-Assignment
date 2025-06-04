2c7ed46ab371ac30c3e58033a388fe50e935c8c9
Parmod Kumar <parmod1feb2002@gmail.com>
2025-06-04 20:48:07 +0530

    Update endPoints.js

--- frontend/src/endpoints/endPoints.js
+++ frontend/src/endpoints/endPoints.js
@@ -1,2 +1,2 @@
-export const RESOURCE_API='http://localhost:8000/api/check-prime';
+export const RESOURCE_API='https://backend-short-assignment.vercel.app/api/check-prime';
 export const FACT_API='https://catfact.ninja/fact';
